1e916202c1ed34d0647500eef787f083e2a48937
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-05-25 17:06:29 +0200

    Header er done med js til at lave burger

---
+++ script.js
@@ -0,0 +1,30 @@
+// Lav variablen "btn", der henviser til ".toggle-btn"
+// Lav variablen "menu", der henviser til ".main-menu"
+
+const btn = document.querySelector(".toggle-btn");
+const btn_img = document.querySelector(".menu-btn");
+const menu = document.querySelector(".main-menu");
+
+// Lav funktionen "toggleMenu()" med følgende funktionalitet
+function toggleMenu() {
+  // a) toggle klassen "shown" på menu vha. classList.toggle
+  menu.classList.toggle("shown");
+
+  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+  const menuShown = menu.classList.contains("shown");
+
+  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+  if (menuShown) {
+    console.log(menuShown); // se i konsollen
+    // sæt btn.textContent til "Luk", hvis menuShown er "true"
+    btn_img.src = "img/svg/close_menu.svg";
+  } else {
+    console.log(menuShown); // se i konsollen
+    // sæt btn.textContent til "Menu", hvis menuShown er "false"
+    btn_img.src = "img/svg/menu.svg";
+  }
+}
+// "toggleMenu()" slutter her
+
+// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+btn.addEventListener("click", toggleMenu);
