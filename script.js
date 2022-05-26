acde27ca47ed3f5ad0195041fdf9e07b980a366c
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-05-26 12:03:33 +0200

    Lavet informationer til "find os" indsat maps kort js til justering + mediaQ

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ function toggleMenu() {
 
 // Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
 btn.addEventListener("click", toggleMenu);
+
+// ======== CONTACT =======
+
+const mapStor = document.querySelector(".mapouter1");
+const mapLille = document.querySelector(".mapouter2");
+
+window.addEventListener("resize", tjekSize);
+
+function tjekSize() {
+  console.log(tjekSize);
+  if (window.matchMedia("(max-width: 1161px)").matches) {
+    console.log("Vinduet er stort nok");
+    mapLille.classList.remove("hidden");
+    mapStor.classList.add("hidden");
+  } else {
+    console.log("Vinduet er for småt");
+    mapLille.classList.add("hidden");
+    mapStor.classList.remove("hidden");
+  }
+}
