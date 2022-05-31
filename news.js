c8b96ed8776112b96a54a62d45ba5d94dd5635f5
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-05-31 12:35:13 +0200

    Så burde der være scroll på knapperne.

---
+++ news.js
@@ -0,0 +1,18 @@
+let cont = document.querySelector(".container-udgivelser");
+let left = document.querySelector(".pre-btn");
+let right = document.querySelector(".nxt-btn");
+
+left.addEventListener("click", slideL);
+right.addEventListener("click", slideR);
+
+function slideL() {
+  console.log("left!");
+  cont.scrollBy(-266, 0);
+  behavior: "smooth";
+}
+
+function slideR() {
+  console.log("Right!");
+  cont.scrollBy(266, 0);
+  behavior: "smooth";
+}
